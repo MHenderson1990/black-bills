ed4c7794171e349606e3a5d9cb8046836cd09076
MHenderson1990 <morganhenderson_90@yahoo.com>
2026-06-25 13:16:22 -0500

    Added Bill Model

---
+++ models/Bill.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const billSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+   
+  },
+  category: {
+    type: String,
+    default: 'Misc.'
+  },
+  isShared: {
+    type: Boolean,
+    default: false
+  },
+  paid: {
+    type: Boolean,
+    default: false
+  },
+});
+
+module.exports = mongoose.model('Bill', billSchema);
