5d50502b8ee802692eedfcae9e0598266f5bdc4a
MHenderson1990 <morganhenderson_90@yahoo.com>
2026-06-25 14:13:03 -0500

    Add SavingsGoal and Contribution models

---
+++ models/SavingsGoal.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const savingsGoalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  isShared: {
+    type: Boolean,
+    default: false,
+  },
+
+  targetAmount: {
+    type: Number,
+   required: true,
+  },
+
+  targetDate: {
+    type: Date,
+  },
+
+  startingBalance: {
+    type: Number,
+    default: 0
+  },
+  
+ owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+ }
+});
+
+module.exports = mongoose.model('SavingsGoal', savingsGoalSchema);
