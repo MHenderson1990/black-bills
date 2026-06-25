f71b99e6a9f3e6b469b183a4aefb5c085dc6ed76
MHenderson1990 <morganhenderson_90@yahoo.com>
2026-06-25 13:05:36 -0500

    Add User Model with password hashing

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  householdId: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  const bcrypt = require('bcryptjs');
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
+
