573e20d1cae2d8b1a1b432dcb878f814ded21607
MHenderson1990 <morganhenderson_90@yahoo.com>
2026-07-13 10:51:24 -0500

    test recalc leftover paycheck

--- server/utils/recalcHousehold.js
+++ server/utils/recalcHousehold.js
@@ -5,11 +5,15 @@ const { computeLeftoverForPaycheck } = require('../controllers/paycheckControlle
 // recalculate the most recent paycheck's leftover for every member of a household
 async function recalcHouseholdLeftovers(householdId) {
   try {
+    console.log('RECALC FIRING for household:', householdId);
     let members = await User.find({ householdId }).select('_id');
+    console.log('RECALC members found:', members.length);
     for (let member of members) {
       let recentPaycheck = await Paycheck.findOne({ earnedBy: member._id }).sort({ date: -1 });
+      console.log('RECALC member', String(member._id), 'paycheck:', recentPaycheck ? recentPaycheck._id : 'NONE');
       if (recentPaycheck) {
-        await computeLeftoverForPaycheck(recentPaycheck);
+        let updated = await computeLeftoverForPaycheck(recentPaycheck);
+        console.log('RECALC updated leftover:', updated ? updated.leftoverAmount : 'no result');
       }
     }
   } catch (error) {
